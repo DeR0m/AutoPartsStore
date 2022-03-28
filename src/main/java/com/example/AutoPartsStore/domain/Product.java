23078fd2cefd1bf8c905463cfb9fbc478a4b1950
DeR0m <denisromantsov@mail.ru>
2022-03-28 10:07:52 +0300

    Init project

---
+++ src/main/java/com/example/AutoPartsStore/domain/Product.java
@@ -0,0 +1,4 @@
+package com.example.AutoPartsStore.domain;
+
+public class Product {
+}
---
+++ src/test/java/com/example/AutoPartsStore/AutoPartsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutoPartsStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPartsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
